93f443cbf8cc7b118dd63b1feb1c5ad0067d1db9
SamLokanc <saml.5@icloud.com>
2024-04-16 20:55:40 -0600

    admin can add/remove comps

--- DinosTeamManager/Frontend/src/components/AdministratorPage.js
+++ DinosTeamManager/Frontend/src/components/AdministratorPage.js
@@ -8,7 +8,8 @@ import DisplayAppBar from "./DisplayAppBar.js";
 import UserInformation from "./UserInformation.js";
 
 import { Paper, Grid, Typography, Table, TableBody, TableCell, TableHead, TableRow,
-         Button, Box, Dialog, DialogContent, TextField, DialogTitle, DialogActions } from '@mui/material';
+         Button, Box, Dialog, DialogContent, TextField, DialogTitle, DialogActions, 
+         DialogContentText, FormControlLabel, Checkbox,  } from '@mui/material';
 
 function AddSwimmerPopup(props) {
     const { open, onClose, setSwimmerData } = props;
@@ -71,8 +72,8 @@ function AddSwimmerPopup(props) {
 
     return (
         <Dialog onClose={handleClose} open={open}>
-            <DialogTitle>Add Swimmer</DialogTitle>
             <DialogContent >
+                <DialogContentText variant="h6" style={{ marginBottom: '20px' }}>Add Swimmer</DialogContentText>
                 <Grid container spacing={2}>
                     <Grid item>
                         <TextField required id="email" label="Email" variant="outlined" InputLabelProps={{shrink: true}}/>
@@ -153,8 +154,8 @@ function AddCoachPopup(props) {
 
     return (
         <Dialog onClose={handleClose} open={open}>
-            <DialogTitle>Add Coach</DialogTitle>
             <DialogContent >
+            <DialogContentText variant="h6" style={{ marginBottom: '20px' }}>Add Coach</DialogContentText>
                 <Grid container spacing={2}>
                     <Grid item>
                         <TextField required id="email" label="Email" variant="outlined" InputLabelProps={{shrink: true}}/>
@@ -183,6 +184,140 @@ function AddCoachPopup(props) {
     )
 }
 
+function AddCompetitionPopup(props) {
+    const {open, onClose, setCompetitionData, setCompetitionNames} = props;
+
+    const handleClose = () => {
+        axios.post('http://localhost:8000/competitions/', {
+            name: document.getElementById('name').value,
+            sanctioned: document.getElementById('sanctioned').checked,
+            start_date: document.getElementById('start_date').value,
+            end_date: document.getElementById('end_date').value,
+        })
+        .then(response => {
+            axios.get('http://localhost:8000/competitions/')  
+                .then(response => {
+                    setCompetitionNames(response.data.map(competition => competition.name));
+                    setCompetitionData(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        });            
+        onClose();
+    };
+
+    return (
+        <Dialog onClose={handleClose} open={open}>
+            <DialogContent>
+                <DialogContentText variant="h6" style={{ marginBottom: '20px' }}>Add Competition</DialogContentText>
+                <Grid container spacing = {2}>
+                    <Grid item xs = {6}>
+                        <TextField required id="name" label="Name" variant="outlined" InputLabelProps={{ shrink: true }} />
+                    </Grid>
+                    <Grid item xs = {6}>
+                        <FormControlLabel
+                            control={<Checkbox required id='sanctioned' />}
+                            label="Sanctioned"
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField required id="start_date" type='date' label="Start Date" variant="outlined" InputLabelProps={{ shrink: true }} />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField required id="end_date" type='date' label="End Date" variant="outlined" InputLabelProps={{ shrink: true }} />
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" onClick={handleClose}>
+                    Add Competition
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+AddCompetitionPopup.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+}
+
+function RemoveCompetitionPopup(props) {
+    const { open, onClose, setCompetitionData, setCompetitionNames, competitionNames} = props;
+    const [competition, setCompetition] = useState('');
+    //const [competitionNames, setCompetitionNames] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/competitions/')
+        .then(response => {
+            const names = response.data.map(competition => competition.name);
+            setCompetitionNames(names);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    const handleClose = () => {
+        axios.delete(`http://localhost:8000/competitions/${competition}`)
+            .then(response => {
+                // Update competition names after deletion
+                setCompetitionNames(prevNames => prevNames.filter(name => name !== competition));
+                axios.get('http://localhost:8000/competitions/')
+                    .then(response => {
+                        setCompetitionData(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        onClose();
+    };
+
+    const handleInputChange = (newValue) => {
+        setCompetition(newValue);
+    };
+
+    return (
+        <Dialog onClose={onClose} open={open}>
+            <DialogContent >
+            <DialogContentText variant="h6" style={{ marginBottom: '20px' }}>Remove Competition</DialogContentText>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <Autocomplete
+                            required
+                            id="competition-select"
+                            options={competitionNames}
+                            value={competition}
+                            onChange={(event, newValue) => handleInputChange(newValue)}
+                            style={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Competitions" variant="outlined" />}
+                        />
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" onClick={handleClose}>
+                    Remove Competition
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+RemoveCompetitionPopup.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+    setCompetitionData: PropTypes.func.isRequired,
+}
+
 
 export default function AdministratorPage(props) {
     const [userData, setUserData] = useState([]);
@@ -190,11 +325,18 @@ export default function AdministratorPage(props) {
     const [swimmerData, setSwimmerData] = useState([]);
     const [coachData, setCoachData] = useState([]);
     const [groupCoachData, setGroupCoachData] = useState([]);
+    const [competitionData, setCompetitionData] = useState([]);
+    const [competitionNames, setCompetitionNames] = useState([]);
+
     const [addSwimmerPopupOpen, setAddSwimmerPopupOpen] = useState(false);
     const [addCoachPopupOpen, setAddCoachPopupOpen] = useState(false);
 
+    const [AddCompetitionPopupOpen, setAddCompetitionPopupOpen] = useState(false);
+    const [RemoveCompetitionPopupOpen, setRemoveCompetitionPopupOpen] = useState(false);
+
     const navigate = useNavigate();
 
+    //add swimmer
     const handleAddSwimmerClickOpen = () => {
         setAddSwimmerPopupOpen(true);
     };
@@ -203,6 +345,7 @@ export default function AdministratorPage(props) {
         setAddSwimmerPopupOpen(false);
     };
 
+    //add coach
     const handleAddCoachClickOpen = () => { 
         setAddCoachPopupOpen(true);
     };
@@ -211,8 +354,30 @@ export default function AdministratorPage(props) {
         setAddCoachPopupOpen(false);
     };
 
+    //add competition
+    const handleAddCompetitionClickOpen = () => { 
+        setAddCompetitionPopupOpen(true);
+    };
+
+    const handleAddCompetitionClickClose = () => {
+        setAddCompetitionPopupOpen(false);
+    };
+
+    //remove competition
+    const handleRemoveCompetitionClickOpen = () => { 
+        setRemoveCompetitionPopupOpen(true);
+    };
+
+    const handleRemoveCompetitionClickClose = () => {
+        setRemoveCompetitionPopupOpen(false);
+    };
+
+
+
     useEffect(() => {
+
         // Navigate back to login page if user not logged in
+        /*
         axios.get("http://localhost:8000/check-login-status/", {withCredentials: true})
         .then(response => {
             if (!response.data['is_authenticated']) {
@@ -245,7 +410,7 @@ export default function AdministratorPage(props) {
         })
         .catch((error) => {
             console.log(error);
-        });
+        });*/
 
         axios.get('http://localhost:8000/swimmers-and-group/')
         .then(response => {
@@ -270,8 +435,16 @@ export default function AdministratorPage(props) {
         .catch((error) => {
             console.log(error);
         });
-        }, []);
-        
+        axios.get(`http://localhost:8000/competitions`)
+        .then(response => {
+            const names = response.data.map(competition => competition.name);
+            setCompetitionNames(names);
+            setCompetitionData(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        }, []);    
 
     return (
         <div>
@@ -355,9 +528,42 @@ export default function AdministratorPage(props) {
                         </Table>
                     </Paper>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={6}>
                     <Paper variant="outlined">
                         <Typography variant='h6'>Competitions</Typography>
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Sanctioned</TableCell>
+                                    <TableCell>Date</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {competitionData.map((competition) => (
+                                    <TableRow key={competition.name}>
+                                        <TableCell>{competition.name}</TableCell>
+                                        <TableCell>{competition.sanctioned ? 'Yes' : 'No'}</TableCell>
+                                        <TableCell>{competition.start_date} to {competition.end_date}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        <Box display="flex" justifyContent="flex-end">
+                            <Button variant="outlined" onClick={handleAddCompetitionClickOpen}>Add Competition</Button>
+                            <Button variant="outlined" onClick={handleRemoveCompetitionClickOpen}>Remove Competition</Button>
+                        </Box>
+                        <AddCompetitionPopup 
+                            open={AddCompetitionPopupOpen} 
+                            onClose={handleAddCompetitionClickClose} 
+                            setCompetitionData={setCompetitionData} 
+                            setCompetitionNames = {setCompetitionNames}/>
+                        <RemoveCompetitionPopup 
+                            open={RemoveCompetitionPopupOpen} 
+                            onClose={handleRemoveCompetitionClickClose} 
+                            setCompetitionData={setCompetitionData} 
+                            setCompetitionNames = {setCompetitionNames}
+                            competitionNames = {competitionNames}/>
                     </Paper>
                 </Grid>
             </Grid>
